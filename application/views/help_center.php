fad271fd0c4ee2670c41cd6eba7f028633e07102
valdimuhammad <valdimuhammad1908@gmail.com>
2021-10-15 20:47:02 +0700

    menambahkan controllers & view custommers-help-center (part1)

---
+++ application/views/help_center.php
@@ -0,0 +1,42 @@
+<!-- Masthead-->
+<header class="masthead">
+	<div class="container position-relative">
+		<div class="row justify-content-center">
+			<div class="col-xl-4">
+				<div class="text-center text-white">
+					<img src="<?=base_url();?>assets/images/resource/cs1.png" alt="" class="image-usephone">
+				</div>
+			</div>
+			<div class="col-xl">
+				<div class="text-left text-white h3 pt-4">
+					<p>How can we help you?</p>
+					<form>
+						<input class="search" type="text" name="search" placeholder="Search for solution">
+					</form>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<!-- Index-->
+<ul class="help_menu">Custommer</ul>
+	<li><a href="#">How to Sign In</a></li>
+	<li><a href="#">I </a></li>
+	<li><a href="#">I Can't Find Something</a></li>
+	<li><a href="#">How to Find Love</a></li>
+	<li><a href="#">Refill Depot can't Call</a></li>
+	<p></p>
+
+<ul class="help_menu">Partners</ul>
+	<li><a href="#">How to Join Us</a></li>
+	<li><a href="#">Customers Can't Find</a></li>
+
+<div class="how-to-sign-in">
+	<li>How To Sign In</li>
+	<p align="justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+	The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>	
+</div>
+
